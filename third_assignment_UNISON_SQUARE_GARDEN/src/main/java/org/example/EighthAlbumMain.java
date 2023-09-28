7354bcdd676533d7ceae50d83f2fd31f08e57eaa
ikeda-Java2307 <2201100202qv@cyber-u.ac.jp>
2023-09-28 13:21:13 +0900

    aaa

--- third_assignment_UNISON_SQUARE_GARDEN/src/main/java/org/example/EighthAlbumMain.java
+++ third_assignment_UNISON_SQUARE_GARDEN/src/main/java/org/example/EighthAlbumMain.java
@@ -5,23 +5,27 @@ import java.util.Map;
 
 public class EighthAlbumMain {
     public static void main(String[] args) {
-        Map<Integer, String> PatrickVegee = new HashMap<>();
-        PatrickVegee.put(1, "Hatch I need");
-        PatrickVegee.put(2, "マーメイドスキャンダラス");
-        PatrickVegee.put(3, "スロウカーブは打てない(that made me crazy)");
-        PatrickVegee.put(4, "Catch up,latency");
-        PatrickVegee.put(5, "摂食ビジランテ");
-        PatrickVegee.put(6, "夏影テールライト");
-        PatrickVegee.put(7, "Phantom Joke");
-        PatrickVegee.put(8, "世界はファンシー");
-        PatrickVegee.put(9, "弥生町ロンリープラネット");
-        PatrickVegee.put(10, "春が来てぼくら");
-        PatrickVegee.put(11, "Simple Simple Anecdote");
-        PatrickVegee.put(12, "101回目のプロローグ");
+        Map<Integer, String> patrickVegee = new HashMap<>();
+        patrickVegee.put(1, "Hatch I need");
+        patrickVegee.put(2, "マーメイドスキャンダラス");
+        patrickVegee.put(3, "スロウカーブは打てない(that made me crazy)");
+        patrickVegee.put(4, "Catch up,latency");
+        patrickVegee.put(5, "摂食ビジランテ");
+        patrickVegee.put(6, "夏影テールライト");
+        patrickVegee.put(7, "Phantom Joke");
+        patrickVegee.put(8, "世界はファンシー");
+        patrickVegee.put(9, "弥生町ロンリープラネット");
+        patrickVegee.put(10, "春が来てぼくら");
+        patrickVegee.put(11, "Simple Simple Anecdote");
+        patrickVegee.put(12, "101回目のプロローグ");
 
         for (int i = 1; i < 14; i++) {
             try {
-                System.out.println(PatrickVegee.get(i));
+                if (patrickVegee.get(i) != null) {
+                    System.out.println(patrickVegee.get(i));
+                } else {
+                    int n = patrickVegee.get(i).length();
+                }
             } catch(NullPointerException e) {
                 System.out.println("存在しない曲を選んでいます");
                 System.out.println("再生を停止します");
